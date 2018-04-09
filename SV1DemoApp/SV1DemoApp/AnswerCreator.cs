b5c2a884d78ab3e18935f168109c73a251ff5ed5
ant2alex <a.schroeer@lua-digital.de>
2018-04-09 14:49:51 +0200

    Added First skeleton for demo

---
+++ SV1DemoApp/SV1DemoApp/AnswerCreator.cs
@@ -0,0 +1,19 @@
+﻿namespace SV1DemoApp
+{
+    internal class AnswerCreator
+    {
+        public string createRandomAnswer()
+        {
+            //TODO: Add body
+
+            return "random answer";
+        }
+
+        public string createConcreteAnswer(string question)
+        {
+            //TODO: Add body
+
+            return "concrete answer";
+        }
+    }
+}
---
+++ SV1DemoApp/SV1DemoApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace SV1DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnswerCreator answerCreator = new AnswerCreator();
+
+            Console.Write("Enter your question or leave empty for random answer: ");
+
+            while (true)
+            {
+                string question = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(question))
+                {
+                    Console.Write(answerCreator.createRandomAnswer());
+                }
+                else if (question.Equals("exit"))
+                {
+                    Console.Write("\n");
+                    Console.Write("Exit");
+                    return;
+                }
+                else
+                {
+                    Console.Write(answerCreator.createConcreteAnswer(question));
+                }
+
+                Console.Write("\n");
+                Console.Write("Enter your question or leave empty for random answer: ");
+            }
+        }
+    }
+}
